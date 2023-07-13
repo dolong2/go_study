e6d9cda81d15dea3e106c29ebb020da86635599f
jeayoung <k01066624566@gmail.com>
2023-07-13 13:45:03 +0900

    add slice example code

--- arr/main/arr.go
+++ arr/main/arr.go
@@ -28,4 +28,24 @@ func main() {
 	//fmt.Println("arr1 == arr2 ->", arr1 == arr2) -> 타입이 일치하지 않아서 에러
 	fmt.Println("arr2 == arr3 ->", arr2 == arr3) // 배열 원소가 일치하지 않아서 false
 	//fmt.Println("arr1 == arr3 ->", arr1 == arr3) -> 타입이 일치하지 않아서 에러
+
+	var slice1 []int
+	var slice2 = make([]int, 4)
+	var slice3 = []int{1, 2, 3, 4}
+	fmt.Println("각 슬라이스의 값")
+	fmt.Println(slice1)
+	fmt.Println(slice2)
+	fmt.Println(slice3)
+	fmt.Println("각 슬라이스의 타입")
+	slice1Type := reflect.TypeOf(slice1)
+	slice2Type := reflect.TypeOf(slice2)
+	slice3Type := reflect.TypeOf(slice3)
+	fmt.Println("arr1 =", slice1Type)
+	fmt.Println("arr2 =", slice2Type)
+	fmt.Println("arr3 =", slice3Type)
+	fmt.Println("각 슬라이스의 타입 비교")
+	fmt.Println("slice1Type == slice2Type ->", slice1Type == slice2Type)
+	fmt.Println("slice2Type == slice3Type ->", slice2Type == slice3Type)
+	fmt.Println("slice1Type == slice3Type ->", slice1Type == slice3Type)
+	fmt.Println("슬라이스는 == 연산자로 비교할 수 없다.")
 }
