0fa739a2bfe0776f362d8c1f7173994f95e1f0e9
jeayoung <k01066624566@gmail.com>
2023-07-19 17:06:50 +0900

    update struct example code

--- struct/main/struct.go
+++ struct/main/struct.go
@@ -23,12 +23,15 @@ func main() {
 	t1.f1 = 1
 	t1.f2 = "Hello"
 	fmt.Println(t1)
+
 	t2 := newTest(10, "hi")
 	fmt.Println(t2)
+
 	t3 := new(Test)
 	t3.f1 = 3
 	t3.f2 = "Hello, world!"
 	fmt.Println(t3)
+
 	fmt.Println("t1 type == ", reflect.TypeOf(t1), "t2 type ==", reflect.TypeOf(t2), "t3 type ==", reflect.TypeOf(t3))
 	// fmt.Println(t1 == t2) 타입이 달라서 에러
 	// fmt.Println(t1 == t3) 타입이 달라서 에러
