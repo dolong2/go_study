d90f03803abddb241dd78962ce2ead54e82726ec
jeayoung <k01066624566@gmail.com>
2023-08-01 16:19:01 +0900

    update channel example code

--- channel/main/channel.go
+++ channel/main/channel.go
@@ -54,4 +54,46 @@ func main() {
 	for i := range ch3 {
 		fmt.Println(i)
 	}
+
+	done1 := make(chan bool)
+	done2 := make(chan bool)
+
+	go run1(done1)
+	go run2(done2)
+
+EXIT:
+	for {
+		select {
+		case <-done1:
+			println("run1 완료")
+
+		case <-done2:
+			println("run2 완료")
+			break EXIT
+		}
+	}
+defaultExit:
+	for {
+		select {
+		case <-done1:
+			println("run1 완료")
+
+		case <-done2:
+			println("run2 완료")
+			break defaultExit
+		default:
+			fmt.Println("default 실행")
+			break defaultExit
+		}
+	}
+}
+
+func run1(done chan bool) {
+	time.Sleep(1 * time.Second)
+	done <- true
+}
+
+func run2(done chan bool) {
+	time.Sleep(2 * time.Second)
+	done <- true
 }
